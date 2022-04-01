57b1a23d592defe167895fb3070ad788c55665c6
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-04-01 17:39:36 +0200

    feat(jwt):adding jwt

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import user from "./user/index.js";
+import auth from "./auth/index.js";
 const router = new Router();
 
 router.use("/user", user);
+router.use("/auth", auth);
 
 export default router;
--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,13 +1,14 @@
-import { request, Router } from "express";
+import { Router } from "express";
+import { validateJWT } from "../../services/jwt/index.js";
 import User from "./model.js";
 
 const router = new Router();
 
-router.get("/", async (req, res) => {
+router.get("/", validateJWT, async (req, res) => {
   return res.json(await User.find({}));
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", validateJWT, async (req, res) => {
   const foundUser = await User.findOne({ _id: req.params.id });
   return foundUser ? res.json(foundUser) : res.sendStatus(404);
 });
@@ -20,7 +21,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", validateJWT, async (req, res) => {
   const foundUser = await User.findOne({ _id: req.params.id });
   if (foundUser) {
     foundUser.set(req.body);
@@ -29,7 +30,7 @@ router.put("/:id", async (req, res) => {
   return foundUser ? res.json(foundUser) : res.sendStatus(404);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", validateJWT, async (req, res) => {
   const result = await User.deleteOne({ _id: req.params.id });
   return result.deletedCount > 0 ? res.sendStatus(204) : res.sendStatus(404);
 });
