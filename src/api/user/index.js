bbdc42b81df7e740572d060e7d8c797a5f47161e
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-04-01 16:25:37 +0200

    feat(): get by id and post

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -1,10 +1,23 @@
-import { Router } from "express";
+import { request, Router } from "express";
 import User from "./model.js";
 
 const router = new Router();
 
 router.get("/", async (req, res) => {
   return res.json(await User.find({}));
+});
+
+router.get("/:id", async (req, res) => {
+  const element = await User.findOne({ _id: request.params.id });
+  return element ? res.json(element) : res.sendStatus(404);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    return res.json(await User.create(req.body));
+  } catch (e) {
+    console.log({ errorPostUser: e });
+  }
 });
 
 export default router;
