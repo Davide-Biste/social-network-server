98b2776bec6d677579206ad68f40e936208cae9d
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-04-01 16:38:56 +0200

    feat():add put and delete user

--- src/api/user/index.js
+++ src/api/user/index.js
@@ -8,8 +8,8 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const element = await User.findOne({ _id: request.params.id });
-  return element ? res.json(element) : res.sendStatus(404);
+  const foundUser = await User.findOne({ _id: req.params.id });
+  return foundUser ? res.json(foundUser) : res.sendStatus(404);
 });
 
 router.post("/", async (req, res) => {
@@ -18,6 +18,20 @@ router.post("/", async (req, res) => {
   } catch (e) {
     console.log({ errorPostUser: e });
   }
+});
+
+router.put("/:id", async (req, res) => {
+  const foundUser = await User.findOne({ _id: req.params.id });
+  if (foundUser) {
+    foundUser.set(req.body);
+    await foundUser.save();
+  }
+  return foundUser ? res.json(foundUser) : res.sendStatus(404);
+});
+
+router.delete("/:id", async (req, res) => {
+  const result = await User.deleteOne({ _id: req.params.id });
+  return result.deletedCount > 0 ? res.sendStatus(204) : res.sendStatus(404);
 });
 
 export default router;
