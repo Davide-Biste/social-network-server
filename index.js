9963b32bb167de6c221e8a6c9aa0c26c7cc44684
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-04-08 10:50:37 +0200

    cors

--- index.js
+++ index.js
@@ -11,16 +11,33 @@ mongoose.set("debug", true);
 
 const app = express();
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(formData.parse());
 app.use(express.urlencoded({ extended: false }));
 
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200, // For legacy browser support
-  methods: "GET, PUT, POST, DELETE",
-};
-
-app.use(cors(corsOptions));
 app.use(api);
 
 app.listen(process.env.PORT || 3000, () => {
