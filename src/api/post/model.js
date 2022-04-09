fc07e4a64862b3eea90010a73e34305de08a3c3e
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-04-09 17:55:30 +0200

    dsa

--- src/api/post/model.js
+++ src/api/post/model.js
@@ -6,6 +6,7 @@ const postSchema = mongoose.Schema({
   uri: { type: String, required: true, default: "loading..." },
   whoPutLike: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
 });
 
 export default mongoose.model("Post", postSchema, "post");
