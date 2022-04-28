8fde85c4e92190df3bc7723697ebc5c430b95472
Davide Mazzeo <dave.mazzeo77@gmail.com>
2022-04-28 16:23:09 +0200

    final change

--- src/services/storage/AWS_s3.js
+++ src/services/storage/AWS_s3.js
@@ -10,47 +10,22 @@ const s3 = new AWS.S3({
 
 export const uploadFile = async (fileName, path, username, id) => {
   try {
-    console.log({ fileName, path, username, id });
+    // Read content from the file
+    const fileContent = fs.readFileSync(path);
+    // Setting up S3 upload parameters
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `${username}/${fileName}`, // File name you want to save as in S3
+      Body: fileContent,
+      ContentType: "image/jpeg",
+    };
 
-    const final = _.trim(path, _.split(path, "/", 7));
-
-    await compress_images(
-      path, //path
-      final + username,
-      { compress_force: false, statistic: true, autoupdate: true },
-      false,
-      { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
-      { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
-      { svg: { engine: "svgo", command: "--multipass" } },
-      {
-        gif: {
-          engine: "gifsicle",
-          command: ["--colors", "64", "--use-col=web"],
-        },
-      },
-      function (err, completed) {
-        if (completed === true) {
-          const newPath = _.split(path, "/", 7);
-          // Read content from the file
-          const fileContent = fs.readFileSync(final + username + newPath[6]);
-          // Setting up S3 upload parameters
-          const params = {
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: `${username}/${fileName}`, // File name you want to save as in S3
-            Body: fileContent,
-            ContentType: "image/jpeg",
-          };
-
-          s3.upload(params, function (err, data) {
-            if (err) {
-              throw err;
-            }
-            fs.rmSync("img/", { recursive: true, force: true });
-            console.log(`File uploaded successfully. ${data.Location}`);
-          });
-        }
+    s3.upload(params, function (err, data) {
+      if (err) {
+        throw err;
       }
-    );
+      console.log(`File uploaded successfully. ${data.Location}`);
+    });
   } catch (e) {
     console.log({ errorUploadImage: e });
   }
